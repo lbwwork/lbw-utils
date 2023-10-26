a1555545d15c48d5f1c17ea8b3c848c5dafb1cd8
libaowen <1139780848@qq.com>
2023-10-26 14:37:36 +0800

    仓库创建

---
+++ src/test/java/com/lbw/lbwutils/LbwUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbw.lbwutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbwUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
